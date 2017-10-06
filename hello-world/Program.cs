04ef2220f18f57bc55d3b930fd6353373c8126b0
Brendan Robb <brendan24k@outlook.com>
2017-10-07 08:04:01 +1000

    conversation

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -12,8 +12,18 @@ namespace hello_world
         {
             Console.Beep();
             Console.WriteLine("Hello World!");
+            Console.WriteLine();
             Console.ReadLine();
-            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Let's play a little game!");
+            Console.ReadLine();
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi {0}! How are you today?", name);
+            string feeling = Console.ReadLine();
+            Console.WriteLine("Yes, well, I'm feeling {0} a bit also... but probably not as "
+                + "much as you!", feeling);
+            Console.WriteLine();
+            Console.WriteLine("{0} is {1}", name, feeling);
             Console.ReadLine();
         }
     }
